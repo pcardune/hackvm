62717cfdf079d04cea19befe23d178d93ec2a973
Paul Carduner <paul@carduner.net>
2023-09-24 14:33:11 -0700

    Add build github actions build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -11,5 +11,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/hackvm",
   plugins: [wasm(), topLevelAwait(), react()],
 });
